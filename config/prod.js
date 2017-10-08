c4d0ffd50117bbc2c6edd95744721512abfc1494
Dino Kraljeta <pockshocks@gmail.com>
2017-10-08 21:38:05 +0200

    fixed bug in environment..comma..jeez

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-	mongoUri: process.env.MONGO_URI
+	mongoUri: process.env.MONGO_URI,
 	cookieKeyOne: process.env.COOKIE_KEY_ONE,
 	cookieKeyTwo: process.env.COOKIE_KEY_TWO,
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
