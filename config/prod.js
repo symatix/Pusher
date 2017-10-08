397b16eb8386598e98a114ab42c30f205fc149d3
Dino Kraljeta <pockshocks@gmail.com>
2017-10-08 20:38:31 +0200

    heroku post-build script added

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,9 @@
 module.exports = {
 	mongoUri: process.env.MONGO_URI
+	cookieKeyOne: process.env.COOKIE_KEY_ONE,
+	cookieKeyTwo: process.env.COOKIE_KEY_TWO,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
 
 // insert all variables in envoironment
